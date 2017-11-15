2a30b759c1f4f042649f3dfbe2816b41eca2677f
Darshan Parajuli <parajulidarshan@gmail.com>
2017-11-15 00:06:26 -0800

    made Log parcelable

--- logcat/src/main/java/com/dp/logcat/Log.kt
+++ logcat/src/main/java/com/dp/logcat/Log.kt
@@ -1,5 +1,8 @@
 package com.dp.logcat
 
+import android.os.Parcel
+import android.os.Parcelable
+
 private var logCounter = 0
 
 data class Log(val id: Int,
@@ -9,7 +12,40 @@ data class Log(val id: Int,
                val tid: String,
                val priority: String,
                val tag: String,
-               val msg: String)
+               val msg: String) : Parcelable {
+
+    override fun writeToParcel(dest: Parcel, flags: Int) {
+        dest.writeInt(id)
+        dest.writeString(date)
+        dest.writeString(time)
+        dest.writeString(pid)
+        dest.writeString(tid)
+        dest.writeString(priority)
+        dest.writeString(tag)
+        dest.writeString(msg)
+    }
+
+    override fun describeContents() = 0
+
+
+    companion object {
+        @JvmField
+        val CREATOR = object : Parcelable.Creator<Log> {
+            override fun createFromParcel(source: Parcel): Log {
+                return Log(source.readInt(),
+                        source.readString(),
+                        source.readString(),
+                        source.readString(),
+                        source.readString(),
+                        source.readString(),
+                        source.readString(),
+                        source.readString())
+            }
+
+            override fun newArray(size: Int) = arrayOfNulls<Log>(size)
+        }
+    }
+}
 
 internal object LogFactory {
     fun createNewLog(metadata: String, msg: String): Log {
