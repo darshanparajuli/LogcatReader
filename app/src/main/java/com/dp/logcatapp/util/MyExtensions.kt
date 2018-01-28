2d196276351f761c3d7210c6dd962f50fa0e0ed5
Darshan Parajuli <parajulidarshan@gmail.com>
2018-01-28 15:29:58 -0800

    fixed BadTokenException crash caused by Toast messages

--- app/src/main/java/com/dp/logcatapp/util/MyExtensions.kt
+++ app/src/main/java/com/dp/logcatapp/util/MyExtensions.kt
@@ -3,19 +3,21 @@ package com.dp.logcatapp.util
 import android.app.Activity
 import android.app.TaskStackBuilder
 import android.content.Context
+import android.content.ContextWrapper
 import android.content.Intent
 import android.content.SharedPreferences
 import android.graphics.Typeface
+import android.os.Build
 import android.support.annotation.LayoutRes
 import android.support.design.widget.Snackbar
 import android.support.v4.app.Fragment
 import android.support.v7.preference.PreferenceManager
-import android.view.LayoutInflater
-import android.view.View
+import android.view.*
 import android.widget.Toast
 import com.dp.logcatapp.R
 import com.dp.logcatapp.activities.MainActivity
 import com.dp.logcatapp.activities.SettingsActivity
+import com.dp.logger.MyLogger
 import java.util.*
 
 //// BEGIN Activity
@@ -59,8 +61,53 @@ fun Fragment.inflateLayout(@LayoutRes layoutResId: Int): View = LayoutInflater.f
 
 private val cache = mutableMapOf<String, Typeface>()
 
-fun Context.showToast(msg: CharSequence, length: Int = Toast.LENGTH_SHORT) =
-        Toast.makeText(this, msg, length).show()
+// Bug find/workaround credit: https://github.com/drakeet/ToastCompat#why
+fun Context.showToast(msg: CharSequence, length: Int = Toast.LENGTH_SHORT) {
+    val toast = Toast.makeText(this, msg, length)
+    if (Build.VERSION.SDK_INT <= 25) {
+        try {
+            val field = View::class.java.getDeclaredField("mContext")
+            field.isAccessible = true
+            field.set(toast.view, ToastViewContextWrapper(this))
+        } catch (e: Exception) {
+        }
+    }
+    toast.show()
+}
+
+private class ToastViewContextWrapper(base: Context) : ContextWrapper(base) {
+    override fun getApplicationContext(): Context =
+            ToastViewApplicationContextWrapper(baseContext.applicationContext)
+}
+
+private class ToastViewApplicationContextWrapper(base: Context) : ContextWrapper(base) {
+    override fun getSystemService(name: String?): Any {
+        return if (name == Context.WINDOW_SERVICE) {
+            ToastWindowManager(baseContext.getSystemService(name) as WindowManager)
+        } else {
+            super.getSystemService(name)
+        }
+    }
+}
+
+private class ToastWindowManager(val base: WindowManager) : WindowManager {
+    override fun getDefaultDisplay(): Display = base.defaultDisplay
+
+    override fun addView(view: View?, params: ViewGroup.LayoutParams?) {
+        try {
+            base.addView(view, params)
+        } catch (e: WindowManager.BadTokenException) {
+            MyLogger.logError("Toast", "caught BadTokenException crash")
+        }
+    }
+
+    override fun updateViewLayout(view: View?, params: ViewGroup.LayoutParams?) =
+            base.updateViewLayout(view, params)
+
+    override fun removeView(view: View?) = base.removeView(view)
+
+    override fun removeViewImmediate(view: View?) = base.removeViewImmediate(view)
+}
 
 fun Context.getTypeface(name: String): Typeface? {
     val assetPath = "fonts/$name.ttf"
