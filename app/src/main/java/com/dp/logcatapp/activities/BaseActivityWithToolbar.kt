9de3b45d439714e715a18b35d72445e0d16644f9
Darshan Parajuli <parajulidarshan@gmail.com>
2018-02-01 21:32:09 -0800

    Manually set color to make the status bar translucent for api >= 21 because setting android:windowTranslucentStatus to true causes a bug that makes activities ignore adjustResize for some reason

--- app/src/main/java/com/dp/logcatapp/activities/BaseActivityWithToolbar.kt
+++ app/src/main/java/com/dp/logcatapp/activities/BaseActivityWithToolbar.kt
@@ -14,6 +14,7 @@ import android.support.v7.preference.PreferenceManager
 import android.support.v7.widget.Toolbar
 import android.util.DisplayMetrics
 import android.view.MenuItem
+import android.view.View
 import com.dp.logcatapp.R
 import com.dp.logcatapp.util.setTheme
 
@@ -33,6 +34,10 @@ abstract class BaseActivityWithToolbar : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         PreferenceManager.setDefaultValues(this, R.xml.settings, false)
         setTheme()
+        if (Build.VERSION.SDK_INT >= 21) {
+            window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
+                    View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+        }
         super.onCreate(savedInstanceState)
     }
 
