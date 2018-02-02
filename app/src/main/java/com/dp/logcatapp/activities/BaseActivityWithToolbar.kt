c412cb1b84cf2dd5f59c9fb0c721cf08652b5915
Darshan Parajuli <parajulidarshan@gmail.com>
2018-02-01 20:00:42 -0800

    use window insets to set appbar paddingTop to status bar height

--- app/src/main/java/com/dp/logcatapp/activities/BaseActivityWithToolbar.kt
+++ app/src/main/java/com/dp/logcatapp/activities/BaseActivityWithToolbar.kt
@@ -2,13 +2,17 @@ package com.dp.logcatapp.activities
 
 import android.annotation.SuppressLint
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import android.os.Handler
+import android.support.design.widget.AppBarLayout
 import android.support.v4.app.NavUtils
+import android.support.v4.view.ViewCompat
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.app.AppCompatDelegate
 import android.support.v7.preference.PreferenceManager
 import android.support.v7.widget.Toolbar
+import android.util.DisplayMetrics
 import android.view.MenuItem
 import com.dp.logcatapp.R
 import com.dp.logcatapp.util.setTheme
@@ -34,8 +38,24 @@ abstract class BaseActivityWithToolbar : AppCompatActivity() {
 
     protected fun setupToolbar() {
         toolbar = findViewById(getToolbarIdRes())
+        if (Build.VERSION.SDK_INT == 19) {
+            setAppBarPaddingForKitkat(toolbar.parent as AppBarLayout)
+        } else if (Build.VERSION.SDK_INT >= 21) {
+            ViewCompat.setOnApplyWindowInsetsListener(toolbar) { view, insets ->
+                val appBarLayout = view.parent as AppBarLayout
+                appBarLayout.setPadding(0, insets.systemWindowInsetTop, 0, 0)
+                insets.consumeSystemWindowInsets()
+            }
+        }
         setSupportActionBar(toolbar)
         supportActionBar?.title = getToolbarTitle()
+    }
+
+    private fun setAppBarPaddingForKitkat(appBarLayout: AppBarLayout) {
+        val dm = DisplayMetrics()
+        windowManager.defaultDisplay.getMetrics(dm)
+        val topPadding = (dm.scaledDensity * 25).toInt()
+        appBarLayout.setPadding(0, topPadding, 0, 0)
     }
 
     protected abstract fun getToolbarIdRes(): Int
