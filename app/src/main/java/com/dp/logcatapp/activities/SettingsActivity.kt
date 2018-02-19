39a637a121565e1b30acbb1ac59a524d787c660c
Darshan Parajuli <parajulidarshan@gmail.com>
2018-02-19 00:11:02 -0800

    replace with SettingsFragment

--- app/src/main/java/com/dp/logcatapp/activities/SettingsActivity.kt
+++ app/src/main/java/com/dp/logcatapp/activities/SettingsActivity.kt
@@ -13,7 +13,7 @@ class SettingsActivity : BaseActivityWithToolbar() {
 
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
-                    .add(R.id.content_frame, SettingsFragment(), SettingsFragment.TAG)
+                    .replace(R.id.content_frame, SettingsFragment(), SettingsFragment.TAG)
                     .commit()
         }
     }
