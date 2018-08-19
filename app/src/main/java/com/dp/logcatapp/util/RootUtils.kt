fd7b0fe5321ba4c052a59913e67a732cd9541adc
Darshan Parajuli <parajulidarshan@gmail.com>
2018-08-18 17:35:41 -0700

    Code cleanup

--- app/src/main/java/com/dp/logcatapp/util/RootUtils.kt
+++ app/src/main/java/com/dp/logcatapp/util/RootUtils.kt
@@ -4,23 +4,20 @@ import android.Manifest
 import android.content.Context
 import com.dp.jshellsession.Config
 import com.dp.jshellsession.JShellSession
-import java.io.IOException
 
 object RootUtils {
 
     fun grantReadLogsPermission(context: Context): Boolean {
-        var jshellSession: JShellSession? = null
+        val cmd = "pm grant ${context.packageName} ${Manifest.permission.READ_LOGS}"
         try {
-            val cmd = "pm grant ${context.packageName} ${Manifest.permission.READ_LOGS}"
             val config = Config.Builder()
                     .setShellCommand("su")
                     .build()
-            jshellSession = JShellSession(config)
-            return jshellSession.run(cmd).exitSuccess()
-        } catch (e: IOException) {
+            return JShellSession(config).use {
+                it.run(cmd).exitSuccess()
+            }
+        } catch (e: Exception) {
             return false
-        } finally {
-            jshellSession?.close()
         }
     }
 }
