38ffe20ffea93de3ed026a9a0547f519508f854b
Darshan Parajuli <parajulidarshan@gmail.com>
2018-10-08 20:38:21 -0700

    Use coroutine to load logs from file

--- app/src/main/java/com/dp/logcatapp/fragments/savedlogsviewer/SavedLogsViewerViewModel.kt
+++ app/src/main/java/com/dp/logcatapp/fragments/savedlogsviewer/SavedLogsViewerViewModel.kt
@@ -2,15 +2,18 @@ package com.dp.logcatapp.fragments.savedlogsviewer
 
 import android.app.Application
 import android.net.Uri
-import android.os.AsyncTask
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import com.dp.logcat.Log
 import com.dp.logcat.LogcatStreamReader
+import com.dp.logcatapp.util.closeQuietly
+import kotlinx.coroutines.experimental.Dispatchers.IO
+import kotlinx.coroutines.experimental.Dispatchers.Main
+import kotlinx.coroutines.experimental.GlobalScope
+import kotlinx.coroutines.experimental.async
+import kotlinx.coroutines.experimental.launch
 import java.io.FileNotFoundException
 import java.io.IOException
-import java.io.InputStream
-import java.lang.ref.WeakReference
 
 internal class SavedLogsViewerViewModel(application: Application) : AndroidViewModel(application) {
     var autoScroll = true
@@ -33,40 +36,28 @@ internal class LogsLiveData(application: Application, uri: Uri) : LiveData<List<
 
     private fun load(application: Application, uri: Uri) {
         try {
-            val inputStream = application.contentResolver.openInputStream(uri)
-            Loader(this).execute(inputStream)
-        } catch (e: FileNotFoundException) {
-            this.value = emptyList()
-        }
-    }
-
-    class Loader(savedLogsLiveData: LogsLiveData) : AsyncTask<InputStream, Void, List<Log>>() {
-
-        private val ref: WeakReference<LogsLiveData> = WeakReference(savedLogsLiveData)
-
-        override fun doInBackground(vararg params: InputStream?): List<Log> {
-            val logs = mutableListOf<Log>()
-
-            val inputStream = params[0]
-            if (inputStream != null) {
-                try {
-                    val reader = LogcatStreamReader(inputStream)
-                    for (log in reader) {
-                        logs += log
+            GlobalScope.launch(Main) {
+                val logs = async(IO) {
+                    val logs = mutableListOf<Log>()
+                    application.contentResolver.openInputStream(uri)?.let {
+                        try {
+                            val reader = LogcatStreamReader(it)
+                            for (log in reader) {
+                                logs += log
+                            }
+                        } catch (e: IOException) {
+                            // ignore
+                        } finally {
+                            it.closeQuietly()
+                        }
                     }
-                } catch (e: IOException) {
-                    // ignore
-                }
-            }
 
-            return logs
-        }
-
-        override fun onPostExecute(result: List<Log>) {
-            val savedLogsLiveData = ref.get()
-            if (savedLogsLiveData != null) {
-                savedLogsLiveData.value = result
+                    logs
+                }.await()
+                value = logs
             }
+        } catch (e: FileNotFoundException) {
+            value = emptyList()
         }
     }
 }
