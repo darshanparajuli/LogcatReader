5ad10281e2bd73d39210bd3f2d6f8c257edbafe0
Darshan Parajuli <parajulidarshan@gmail.com>
2018-10-29 22:03:13 -0700

    Update permission instructions dialog

--- app/src/main/java/com/dp/logcatapp/fragments/logcatlive/dialogs/InstructionToGrantPermissionDialogFragment.kt
+++ app/src/main/java/com/dp/logcatapp/fragments/logcatlive/dialogs/InstructionToGrantPermissionDialogFragment.kt
@@ -5,30 +5,21 @@ import android.app.Dialog
 import android.content.ClipData
 import android.content.ClipboardManager
 import android.content.Context
-import android.content.Intent
 import android.os.Bundle
-import android.os.Process
 import androidx.appcompat.app.AlertDialog
 import com.dp.logcatapp.R
 import com.dp.logcatapp.fragments.base.BaseDialogFragment
-import com.dp.logcatapp.services.LogcatService
-import com.dp.logcatapp.util.RootUtils
-import com.dp.logcatapp.util.showToast
+import com.dp.logcatapp.util.inflateLayout
 
 class InstructionToGrantPermissionDialogFragment : BaseDialogFragment() {
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        val view = inflateLayout(R.layout.permission_instruction)
         return AlertDialog.Builder(activity!!)
                 .setTitle(R.string.read_logs_permission_required)
-                .setMessage(R.string.grant_read_logs_permission_instruction)
-                .setPositiveButton(R.string.grant_read_logs_permission_action_using_root) { _, _ ->
-                    if (RootUtils.grantReadLogsPermission(activity!!)) {
-                        AppClosingDialog().show(fragmentManager, AppClosingDialog.TAG)
-                    } else {
-                        activity!!.showToast(getString(R.string.failed))
-                    }
-                }
-                .setNegativeButton(R.string.copy_to_clipboard) { _, _ ->
+                .setView(view)
+                .setPositiveButton(android.R.string.ok, null)
+                .setNeutralButton(R.string.copy_to_clipboard) { _, _ ->
                     val cmd = "adb shell pm grant ${activity!!.packageName} " +
                             Manifest.permission.READ_LOGS
                     val cm = activity!!.getSystemService(Context.CLIPBOARD_SERVICE)
@@ -39,32 +30,7 @@ class InstructionToGrantPermissionDialogFragment : BaseDialogFragment() {
                 .create()
     }
 
-    internal class AppClosingDialog : BaseDialogFragment() {
-        override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
-            return AlertDialog.Builder(activity!!)
-                    .setTitle(R.string.app_name)
-                    .setMessage(getString(R.string.app_kill_reason))
-                    .setPositiveButton(getString(R.string.kill)) { _, _ ->
-                        killApp()
-                    }
-                    .setOnDismissListener {
-                        killApp()
-                    }
-                    .create()
-        }
-
-        private fun killApp() {
-            activity!!.stopService(Intent(activity!!, LogcatService::class.java))
-            Process.killProcess(Process.myPid())
-        }
-
-        companion object {
-            val TAG = AppClosingDialog::class.qualifiedName
-        }
-    }
-
     companion object {
         val TAG = InstructionToGrantPermissionDialogFragment::class.qualifiedName
     }
-
 }
