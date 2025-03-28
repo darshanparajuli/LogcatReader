1801450b01c7018cb4cc422beb87fb56145f9bf3
Darshan Parajuli <parajulidarshan@gmail.com>
2025-03-28 09:50:11 -0700

    More cleanup - remove dead code

--- app/src/main/java/com/dp/logcatapp/ui/theme/Type.kt
+++ app/src/main/java/com/dp/logcatapp/ui/theme/Type.kt
@@ -5,21 +5,10 @@ import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontStyle.Companion.Italic
 import androidx.compose.ui.text.font.FontWeight.Companion.Bold
-import androidx.compose.ui.text.font.FontWeight.Companion.Light
 import androidx.compose.ui.text.font.FontWeight.Companion.Medium
 import com.dp.logcatapp.R
 
 val AppTypography = Typography()
-
-val RobotoFontFamily = FontFamily(
-  Font(resId = R.font.roboto_regular),
-  Font(resId = R.font.roboto_italic, style = Italic),
-  Font(resId = R.font.roboto_light, weight = Light),
-  Font(resId = R.font.roboto_medium, weight = Medium),
-  Font(resId = R.font.roboto_medium_italic, weight = Medium, style = Italic),
-  Font(resId = R.font.roboto_bold, weight = Bold),
-  Font(resId = R.font.roboto_bold_italic, weight = Bold, style = Italic),
-)
 
 val RobotoMonoFontFamily = FontFamily(
   Font(resId = R.font.roboto_mono_regular),
@@ -28,11 +17,4 @@ val RobotoMonoFontFamily = FontFamily(
   Font(resId = R.font.roboto_mono_medium_italic, weight = Medium, style = Italic),
   Font(resId = R.font.roboto_mono_bold, weight = Bold),
   Font(resId = R.font.roboto_mono_bold_italic, weight = Bold, style = Italic),
-)
-
-val RobotoCondensedFontFamily = FontFamily(
-  Font(resId = R.font.roboto_condensed_regular),
-  Font(resId = R.font.roboto_condensed_italic, style = Italic),
-  Font(resId = R.font.roboto_condensed_bold, weight = Bold),
-  Font(resId = R.font.roboto_condensed_bold_italic, weight = Bold, style = Italic),
 )
