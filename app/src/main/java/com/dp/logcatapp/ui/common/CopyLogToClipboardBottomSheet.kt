a73ff4184d532f475b5e96c79a09d032942795e0
Darshan Parajuli <parajulidarshan@gmail.com>
2025-04-01 16:12:52 -0700

    Add option to copy the whole log

--- app/src/main/java/com/dp/logcatapp/ui/common/CopyLogToClipboardBottomSheet.kt
+++ app/src/main/java/com/dp/logcatapp/ui/common/CopyLogToClipboardBottomSheet.kt
@@ -1,7 +1,10 @@
 package com.dp.logcatapp.ui.common
 
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ListItem
 import androidx.compose.material3.ListItemDefaults
@@ -14,7 +17,6 @@ import androidx.compose.ui.platform.LocalClipboardManager
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.style.TextOverflow
-import androidx.compose.ui.util.fastForEachIndexed
 import com.dp.logcat.Log
 import com.dp.logcatapp.R
 import com.dp.logcatapp.ui.theme.AppTypography
@@ -30,57 +32,51 @@ fun CopyLogClipboardBottomSheet(
     onDismissRequest = onDismiss,
     containerColor = MaterialTheme.colorScheme.surfaceContainer,
   ) {
-    ListItem(
-      modifier = Modifier.fillMaxWidth(),
-      headlineContent = {
-        Text(
-          modifier = Modifier.weight(1f),
-          text = stringResource(R.string.copy_to_clipboard),
-          style = AppTypography.titleMedium,
-        )
-      },
-      colors = ListItemDefaults.colors(
-        containerColor = MaterialTheme.colorScheme.surfaceContainer,
-      ),
-    )
-    val labels = listOf(
-      stringResource(R.string.tag),
-      stringResource(R.string.message),
-      stringResource(R.string.date),
-      stringResource(R.string.time),
-      stringResource(R.string.process_id),
-      stringResource(R.string.thread_id),
-    )
-    val data = listOf(
-      log.tag,
-      log.msg,
-      log.date,
-      log.time,
-      log.pid,
-      log.tid,
-    )
-    labels.zip(data).fastForEachIndexed { index, (label, data) ->
+    Column(modifier = Modifier.verticalScroll(rememberScrollState())) {
       ListItem(
-        modifier = Modifier
-          .fillMaxWidth()
-          .clickable {
-            clipboard.setText(AnnotatedString(data))
-            onDismiss()
-          },
+        modifier = Modifier.fillMaxWidth(),
         headlineContent = {
-          Text(label)
-        },
-        supportingContent = {
           Text(
-            text = data,
-            maxLines = 1,
-            overflow = TextOverflow.Ellipsis,
+            modifier = Modifier.weight(1f),
+            text = stringResource(R.string.copy_to_clipboard),
+            style = AppTypography.titleMedium,
           )
         },
         colors = ListItemDefaults.colors(
           containerColor = MaterialTheme.colorScheme.surfaceContainer,
         ),
       )
+      listOf(
+        R.string.log to log.toString(),
+        R.string.tag to log.tag,
+        R.string.message to log.msg,
+        R.string.date to log.date,
+        R.string.time to log.time,
+        R.string.process_id to log.pid,
+        R.string.thread_id to log.tid,
+      ).forEach { (labelRes, data) ->
+        ListItem(
+          modifier = Modifier
+            .fillMaxWidth()
+            .clickable {
+              clipboard.setText(AnnotatedString(data))
+              onDismiss()
+            },
+          headlineContent = {
+            Text(stringResource(labelRes))
+          },
+          supportingContent = {
+            Text(
+              text = data,
+              maxLines = if (labelRes == R.string.log) 2 else 1,
+              overflow = TextOverflow.Ellipsis,
+            )
+          },
+          colors = ListItemDefaults.colors(
+            containerColor = MaterialTheme.colorScheme.surfaceContainer,
+          ),
+        )
+      }
     }
   }
 }
