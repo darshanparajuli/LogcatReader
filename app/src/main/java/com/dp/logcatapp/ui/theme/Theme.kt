4e7788d0e7ba55dfc3628ccfad955df81b712dfb
Darshan Parajuli <parajulidarshan@gmail.com>
2025-04-09 16:07:10 -0700

    Fix secondary log list color based on theme setting

--- app/src/main/java/com/dp/logcatapp/ui/theme/Theme.kt
+++ app/src/main/java/com/dp/logcatapp/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package com.dp.logcatapp.ui.theme
 
-import android.content.SharedPreferences
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
@@ -9,17 +8,12 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.DisposableEffect
-import androidx.compose.runtime.ReadOnlyComposable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import com.dp.logcatapp.util.PreferenceKeys
 import com.dp.logcatapp.util.PreferenceKeys.Appearance.Theme
-import com.dp.logcatapp.util.getDefaultSharedPreferences
+import com.dp.logcatapp.util.rememberBooleanSharedPreference
+import com.dp.logcatapp.util.rememberStringSharedPreference
 
 private val lightScheme = lightColorScheme(
   primary = primaryLight,
@@ -107,47 +101,15 @@ fun LogcatReaderTheme(
 ) {
 
   val context = LocalContext.current
-  var appThemeSetting by remember(context) {
-    mutableStateOf(
-      context.getDefaultSharedPreferences()
-        .getString(PreferenceKeys.Appearance.KEY_THEME, PreferenceKeys.Appearance.Default.THEME)
-    )
-  }
+  var appThemeSetting = rememberStringSharedPreference(
+    key = PreferenceKeys.Appearance.KEY_THEME,
+    default = PreferenceKeys.Appearance.Default.THEME,
+  ).value
 
-  var dynamicColor by remember(context) {
-    mutableStateOf(
-      context.getDefaultSharedPreferences()
-        .getBoolean(
-          PreferenceKeys.Appearance.KEY_DYNAMIC_COLOR,
-          PreferenceKeys.Appearance.Default.DYNAMIC_COLOR
-        )
-    )
-  }
-
-  DisposableEffect(context) {
-    val preferences = context.getDefaultSharedPreferences()
-    val listener = SharedPreferences.OnSharedPreferenceChangeListener { prefs, key ->
-      when (key) {
-        PreferenceKeys.Appearance.KEY_THEME -> {
-          appThemeSetting = prefs.getString(
-            PreferenceKeys.Appearance.KEY_THEME,
-            PreferenceKeys.Appearance.Default.THEME
-          )
-        }
-        PreferenceKeys.Appearance.KEY_DYNAMIC_COLOR -> {
-          dynamicColor = preferences.getBoolean(
-            PreferenceKeys.Appearance.KEY_DYNAMIC_COLOR,
-            PreferenceKeys.Appearance.Default.DYNAMIC_COLOR
-          )
-        }
-      }
-    }
-
-    preferences.registerOnSharedPreferenceChangeListener(listener)
-    onDispose {
-      preferences.unregisterOnSharedPreferenceChangeListener(listener)
-    }
-  }
+  var dynamicColor = rememberBooleanSharedPreference(
+    key = PreferenceKeys.Appearance.KEY_DYNAMIC_COLOR,
+    default = PreferenceKeys.Appearance.Default.DYNAMIC_COLOR
+  ).value
 
   val colorScheme = when (appThemeSetting) {
     Theme.AUTO -> {
@@ -182,12 +144,23 @@ fun LogcatReaderTheme(
 
 fun isDynamicThemeAvailable() = Build.VERSION.SDK_INT >= 31
 
-@ReadOnlyComposable
 @Composable
 fun currentSearchHitColor(): Color =
-  if (isSystemInDarkTheme()) currentSearchHitColorDark else currentSearchHitColorLight
+  if (isDarkThemeOn()) currentSearchHitColorDark else currentSearchHitColorLight
 
-@ReadOnlyComposable
 @Composable
 fun logListItemSecondaryColor(): Color =
-  if (isSystemInDarkTheme()) logListItemSecondaryColorDark else logListItemSecondaryColorLight
+  if (isDarkThemeOn()) logListItemSecondaryColorDark else logListItemSecondaryColorLight
+
+@Composable
+private fun isDarkThemeOn(): Boolean {
+  var appThemeSetting = rememberStringSharedPreference(
+    key = PreferenceKeys.Appearance.KEY_THEME,
+    default = PreferenceKeys.Appearance.Default.THEME,
+  ).value
+  return if (appThemeSetting == Theme.AUTO) {
+    isSystemInDarkTheme()
+  } else {
+    appThemeSetting == Theme.DARK
+  }
+}
