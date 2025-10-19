1c3b1ebe8ab1903d338259ed4d664ea012db504d
Darshan Parajuli <parajulidarshan@gmail.com>
2025-10-19 10:07:32 -0700

    Use plain tool tip

--- app/src/main/java/com/dp/logcatapp/ui/common/WithTooltip.kt
+++ app/src/main/java/com/dp/logcatapp/ui/common/WithTooltip.kt
@@ -1,8 +1,9 @@
 package com.dp.logcatapp.ui.common
 
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.RichTooltip
+import androidx.compose.material3.PlainTooltip
 import androidx.compose.material3.Text
+import androidx.compose.material3.TooltipAnchorPosition.Companion.Above
 import androidx.compose.material3.TooltipBox
 import androidx.compose.material3.TooltipDefaults
 import androidx.compose.material3.rememberTooltipState
@@ -18,9 +19,11 @@ fun WithTooltip(
 ) {
   TooltipBox(
     modifier = modifier,
-    positionProvider = TooltipDefaults.rememberRichTooltipPositionProvider(),
+    positionProvider = TooltipDefaults.rememberTooltipPositionProvider(
+      positioning = Above,
+    ),
     tooltip = {
-      RichTooltip {
+      PlainTooltip {
         Text(text)
       }
     },
