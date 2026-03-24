b14aea4c00ecf6aeae838538f07b39aaa95d5b93
Darshan Parajuli <parajulidarshan@gmail.com>
2026-03-24 13:10:47 -0700

    Fix search highlight for the current search hit not working as expected
    The issue was the hit indices was not getting set correctly. To fix it, we sort the search result by log id to ensure the hit indices ordering aligns with the original logs order

--- app/src/main/java/com/dp/logcatapp/util/SearchLogs.kt
+++ app/src/main/java/com/dp/logcatapp/util/SearchLogs.kt
@@ -67,25 +67,48 @@ suspend fun searchLogs(
   if (logs.size > CHUNKED_SEARCH_THRESHOLD) {
     val chunkSize = logs.size / (Runtime.getRuntime().availableProcessors() - 1)
       .coerceAtLeast(1)
+    val spans = mutableListOf<SearchHitSpanData>()
     logs.asSequence()
       .withIndex()
       .chunked(chunkSize)
       .map { logs ->
         async {
-          logs.forEach { (index, log) ->
+          logs.map { (index, log) ->
             ensureActive()
-            performSearch(
+            SearchHitSpanData(
               index = index,
-              log = log,
-              appInfoMap = appInfoMap,
-              searchFunction = searchFunction,
-              hits = hits,
-              map = map,
+              logId = log.id,
+              tag = searchFunction(log.tag),
+              msg = searchFunction(log.msg),
+              packageName = log.uid?.let { uid ->
+                val packageName = if (uid.isNum) {
+                  appInfoMap[uid.value]?.packageName
+                } else {
+                  uid.value
+                }
+                packageName?.let { searchFunction(it) }.orEmpty()
+              } ?: emptySequence(),
+              date = searchFunction(log.date),
+              time = searchFunction(log.time),
+              pid = searchFunction(log.pid),
+              tid = searchFunction(log.tid),
             )
           }
         }
       }.toList()
       .awaitAll()
+      .forEach { spans += it }
+
+    ensureActive()
+
+    spans.sortBy { it.logId }
+    spans.forEach { spanData ->
+      addSpans(
+        spanData = spanData,
+        hits = hits,
+        map = map,
+      )
+    }
   } else {
     logs.forEachIndexed { index, log ->
       ensureActive()
@@ -113,45 +136,114 @@ private fun performSearch(
   hits: MutableList<SearchHit>,
   map: MutableMap<SearchHitKey, List<HitIndex>>,
 ) {
-  val tagSearchResult = searchFunction(log.tag)
-  val msgSearchResult = searchFunction(log.msg)
-  val packageNameSearchResult = log.uid?.let { uid ->
-    val packageName = if (uid.isNum) {
-      appInfoMap[uid.value]?.packageName
-    } else {
-      uid.value
-    }
-    packageName?.let { searchFunction(it) }.orEmpty()
-  } ?: emptySequence()
-  val dateSearchResult = searchFunction(log.date)
-  val timeSearchResult = searchFunction(log.time)
-  val pidSearchResult = searchFunction(log.pid)
-  val tidSearchResult = searchFunction(log.tid)
+  addSpans(
+    spanData = SearchHitSpanData(
+      index = index,
+      logId = log.id,
+      tag = searchFunction(log.tag),
+      msg = searchFunction(log.msg),
+      packageName = log.uid?.let { uid ->
+        val packageName = if (uid.isNum) {
+          appInfoMap[uid.value]?.packageName
+        } else {
+          uid.value
+        }
+        packageName?.let { searchFunction(it) }.orEmpty()
+      } ?: emptySequence(),
+      date = searchFunction(log.date),
+      time = searchFunction(log.time),
+      pid = searchFunction(log.pid),
+      tid = searchFunction(log.tid),
+    ),
+    hits = hits,
+    map = map,
+  )
+}
 
-  fun addSpans(
-    spans: Sequence<SearchHitSpan>,
-    component: LogComponent,
-  ) {
-    val hitIndices = mutableListOf<HitIndex>()
-    spans.forEach { span ->
-      hitIndices += HitIndex(hits.size)
-      hits += SearchHit(
-        index = index,
-        span = span,
-      )
-    }
-    if (hitIndices.isNotEmpty()) {
-      map[SearchHitKey(logId = log.id, component = component)] = hitIndices
-    }
+private fun addSpans(
+  spanData: SearchHitSpanData,
+  hits: MutableList<SearchHit>,
+  map: MutableMap<SearchHitKey, List<HitIndex>>,
+) {
+  val index = spanData.index
+  val logId = spanData.logId
+  addSpans(
+    spans = spanData.tag,
+    component = LogComponent.Tag,
+    index = index,
+    logId = logId,
+    hits = hits,
+    map = map
+  )
+  addSpans(
+    spans = spanData.msg,
+    component = LogComponent.Message,
+    index = index,
+    logId = logId,
+    hits = hits,
+    map = map
+  )
+  addSpans(
+    spans = spanData.packageName,
+    component = LogComponent.PackageName,
+    index = index,
+    logId = logId,
+    hits = hits,
+    map = map
+  )
+  addSpans(
+    spans = spanData.date,
+    component = LogComponent.Date,
+    index = index,
+    logId = logId,
+    hits = hits,
+    map = map
+  )
+  addSpans(
+    spans = spanData.time,
+    component = LogComponent.Time,
+    index = index,
+    logId = logId,
+    hits = hits,
+    map = map
+  )
+  addSpans(
+    spans = spanData.pid,
+    component = LogComponent.Pid,
+    index = index,
+    logId = logId,
+    hits = hits,
+    map = map
+  )
+  addSpans(
+    spans = spanData.tid,
+    component = LogComponent.Tid,
+    index = index,
+    logId = logId,
+    hits = hits,
+    map = map
+  )
+}
+
+private fun addSpans(
+  spans: Sequence<SearchHitSpan>,
+  component: LogComponent,
+  index: Int,
+  logId: Int,
+  hits: MutableList<SearchHit>,
+  map: MutableMap<SearchHitKey, List<HitIndex>>,
+) {
+  val hitIndices = mutableListOf<HitIndex>()
+  spans.forEach { span ->
+    hitIndices += HitIndex(hits.size)
+    hits += SearchHit(
+      index = index,
+      span = span,
+    )
   }
-
-  addSpans(tagSearchResult, LogComponent.Tag)
-  addSpans(msgSearchResult, LogComponent.Message)
-  addSpans(packageNameSearchResult, LogComponent.PackageName)
-  addSpans(dateSearchResult, LogComponent.Date)
-  addSpans(timeSearchResult, LogComponent.Time)
-  addSpans(pidSearchResult, LogComponent.Pid)
-  addSpans(tidSearchResult, LogComponent.Tid)
+  if (hitIndices.isNotEmpty()) {
+    map[SearchHitKey(logId = logId, component = component)] = hitIndices
+  }
 }
 
 private fun search(
@@ -187,6 +279,18 @@ private fun search(
     SearchHitSpan(start = result.range.first, end = result.range.last + 1)
   }
 }
+
+private data class SearchHitSpanData(
+  val index: Int,
+  val logId: Int,
+  val tag: Sequence<SearchHitSpan>,
+  val msg: Sequence<SearchHitSpan>,
+  val packageName: Sequence<SearchHitSpan>,
+  val date: Sequence<SearchHitSpan>,
+  val time: Sequence<SearchHitSpan>,
+  val pid: Sequence<SearchHitSpan>,
+  val tid: Sequence<SearchHitSpan>,
+)
 
 data class SearchResult(
   val hitIndexMap: Map<SearchHitKey, List<HitIndex>>,
